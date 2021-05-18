3ca29a56799db219622b33cdf5c71f87642a0804
hyywestwood <3140105713@zju.edu.cn>
2021-05-18 12:39:45 +0800

    尝试添加足球视频下载

---
+++ football.py
@@ -0,0 +1,13 @@
+import requests
+import json
+import os, re
+from contextlib import closing
+from bs4 import BeautifulSoup
+import subprocess
+
+
+
+if __name__ == '__main__':
+    # 尝试读取m3u8文件
+    pass
+    
